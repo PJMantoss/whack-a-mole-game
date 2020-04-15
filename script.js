eef4b073143df9db9d791bb95db2c562eaa53c25
pjmantoss <joelptoss@gmail.com>
2020-04-15 19:17:47 +0100

    update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const mole = document.querySelectorAll('.mole');
 const timeLeft = document.getElementById('time-left');
 
 let result = 0;
+
+const randomSquare = () => {
+    square.forEach(className => {
+        className.classList.remove('mole');
+    })
+
+    let randomPosition = square[Math.floor(Math.random() * 9)];
+    randomPosition.classList.add('mole');
+}
